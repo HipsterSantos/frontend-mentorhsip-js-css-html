a6a640c4d98e8143e4860c88a73130e4ec44d95d
HipsterSantos <santoscampos269@gmail.com>
2024-07-04 21:58:43 +0100

    [update ]for loop

---
+++ javascript-module/for.loop.js
@@ -0,0 +1,46 @@
+length = length -1;
+
+// (6) [1, 2, 3, 3, 4, 54]
+
+for (let from = 0; from < studentScore.length -1; from)
+// VM1113:1 Uncaught SyntaxError: Unexpected end of input
+for (let from = 0; from < studentScore.length -1; from = from + 1){
+    console.log(`The last item is   ${studentScore[studentScore.length-1]}`)
+
+}
+// 5VM1247:2 The last item is   54
+
+var len = studentScore.length
+for (let from = 0; from < studentScore.length -1; from = from + 1){
+    console.log(`The last item is   ${studentScore[studentScore.length-1]}`)
+
+}
+
+
+var len = studentScore.length -1;
+
+for (let from = 0; from < studentScore.length -1; from = from + 1){
+    console.log(`The last item is   ${studentScore[len]}`)
+    len = len -1;
+
+}
+
+
+
+const arr1 = [2,3,3,4];
+const arr2 = [83882,3838];
+const medium = 0;
+
+Array(arr1,arr2)
+// (2) [Array(4), Array(2)]0: (4) [2, 3, 3, 4]1: (2) [83882, 3838]length: 2[[Prototype]]: Array(0)
+// [1,2,2,3,34]
+
+var objj = [22,"string some:", true, {
+    name: "something"
+}]
+
+var mult = [  [], [] ]
+
+var mult = [  [1,2,3], [,3,4] ]
+
+
